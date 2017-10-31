a069758ecbcff50b792015b38697a560cbba588c
SvetaZlobina <svetlanazlobina97@gmail.com>
2017-10-31 16:02:39 +0300

    progress bar in login fragment + noHistory login activity

--- app/src/main/java/com/koala/infinitum/android_project/loginFragments/LoginFragment.java
+++ app/src/main/java/com/koala/infinitum/android_project/loginFragments/LoginFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.koala.infinitum.android_project.MainActivity;
@@ -26,6 +27,7 @@ public class LoginFragment extends Fragment{
     private EditText login_text;
     private EditText password_text;
     private Button login_btn;
+    private ProgressBar progressBar;
 
     @Nullable
     @Override
@@ -37,16 +39,19 @@ public class LoginFragment extends Fragment{
         login_text = (EditText) view.findViewById(R.id.login);
         password_text = (EditText) view.findViewById(R.id.password);
         login_btn = (Button) view.findViewById(R.id.login_btn);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
 
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
                 new LoginService().login(
                         login_text.getText().toString(),
                         password_text.getText().toString(),
                         new ClientCallback<ResponseOneObject<UserValidation>>() {
                     @Override
                     public void onSuccess(retrofit2.Response<ResponseOneObject<UserValidation>> response) {
+                        progressBar.setVisibility(View.GONE);
                         Intent intent = new Intent(getActivity(), MainActivity.class);
                         intent.putExtra("token", response.body().getData().getToken());
                         startActivity(intent);
@@ -54,6 +59,7 @@ public class LoginFragment extends Fragment{
 
                     @Override
                     public void onError(String err) {
+                        progressBar.setVisibility(View.GONE);
                         Toast.makeText(getActivity(), err, Toast.LENGTH_LONG).show();
                         Log.d("login_error", err);
                     }
